d0ce8b0169b16fc63b19614171cdb3ab645eead5
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2022-01-11 23:03:36 +0900

    주석 Commit

--- routers/router.ts
+++ routers/router.ts
@@ -2,7 +2,6 @@ import {router,Route,} from '../.core/Router___.ts'
 import View from '../.core/view___.ts'
 import { ControllerRoute as Controller } from '../.core/controller___.ts'
 
-
 Route.get("/", async(ctx) => {
     return await View.make(ctx, "index");
 })
@@ -11,8 +10,6 @@ Route.get("/hello", await Controller.set("DefaultController", "hello"));
 Route.get("/gf", await Controller.set("DefaultController", "show"));
 Route.get("/webconfig", await Controller.set("ConfigController/controller", "get"))
 Route.get("/webconfig/create", await Controller.set("ConfigController/controller", "create"))
-
-
 
 let listrouter = async() => {
     return Route.ShowRoute()
