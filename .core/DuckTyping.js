f320dd74da77f327b6bead93ac1ac93a4f98144b
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2022-01-22 14:48:21 +0900

    DuckTyping commit

--- .core/DuckTyping.js
+++ .core/DuckTyping.js
@@ -4,4 +4,14 @@
         console.log(d);
         return [+d.x, +d.y];
     })
+
+    function XYPair(x, y){
+        this.x = x;
+        this.y= y;
+    }
+    objData = [
+        new XYPair(10, 130),
+        new XYPair(20, 130),
+        new XYPair(30, 130)
+    ]
 })
